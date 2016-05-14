17d34dd63b6cd0b61389284ed85ff4a1726b4c68
Davide Zanotti <davidezanotti@gmail.com>
2016-05-14 19:06:55 +0200

    fixed and improved nvm installation + jspm

--- src/languages/javascript.sh
+++ src/languages/javascript.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
 echo "Installing nodejs and common frontend development tools..."
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+# download nvm
+git clone https://github.com/creationix/nvm.git ~/.nvm
+cd ~/.nvm
+git checkout `git describe --abbrev=0 --tags`
+
+# set variables
+export NVM_DIR="/home/dave/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+
+# update .bashrc
+cat << 'END' >> ~/.bashrc
+
+# nvm settings:
+export NVM_DIR="/home/dave/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+END
+
+# install node/npm
 nvm install stable
+
+# install npm packages
+npm install -g jspm
 npm install -g gulp
 npm install -g grunt-cli
 npm install -g bower
 npm install -g phantomjs
-echo "Node, NPM, gulp, grunt, phantomjs and bower have been installed"
+
+echo "Node, NPM, jspm, gulp, grunt, phantomjs and bower have been installed"
