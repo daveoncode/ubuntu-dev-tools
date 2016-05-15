a859f2129683943e6e954d656a35d401872069af
Davide Zanotti <davidezanotti@gmail.com>
2016-05-15 11:09:23 +0200

    aptl alias to list installed packages

--- src/aliases/apt.sh
+++ src/aliases/apt.sh
@@ -7,6 +7,7 @@ cat << 'END' >> ~/.bashrc
 
 # apt-get aliases:
 alias apti="sudo apt-get install -y"
+alias aptl="apt list --installed"
 alias aptr="sudo apt-get remove"
 alias aptp="sudo apt-get purge"
 alias aptu="sudo apt-get update"
@@ -14,4 +15,4 @@ alias apts="apt-cache search --names-only"
 
 END
 
-echo 'The following aliases have been created: apti, aptr, aptp, aptu, apts'
+echo 'The following aliases have been created: apti, aptl, aptr, aptp, aptu, apts'
