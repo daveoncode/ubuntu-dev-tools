1ebb94ee40971affb91a593d0ca447028e65f527
Davide Zanotti <davidezanotti@gmail.com>
2016-05-15 11:08:49 +0200

    docker user group fix

--- src/tools/docker.sh
+++ src/tools/docker.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Installing Docker..."
+
+# add repo
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
+
+# install docker
 apt-get update
 apt-get purge lxc-docker
 apt-cache policy docker-engine
 apt-get install -y linux-image-extra-$(uname -r) docker-engine docker-compose docker-registry
-usermod -aG docker $(whoami)
+
+# add current user to docker group
+usermod -aG docker $(who | awk '{print $1}')
+
+# start docker
 service docker start
+
 echo "Docker is up and running!"
